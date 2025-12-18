f3df653111cf1a50569953998a89306b59434026
Dhaval Padhiyar <dhavalpadhiyar18112004@gmail.com>
2025-12-18 18:31:19 +0530

    Add files via upload

---
+++ Inbuilt_Led_Blink/Inbuilt_Led_Blink (Bare_metal).ino
@@ -0,0 +1,12 @@
+void setup()
+{ // inbuilt led of controller is connected to the PB5 pin
+  DDRB|=(1<<DDB5);// setting PB% as an output port
+}
+
+void loop()
+{
+  PORTB|=(1<<PB5); //PB5 as high
+  delay(1000); //1s wait
+  PORTB&=~(1<<PB5);//PB5 as low 
+  delay(1000);// 1s wait
+}
