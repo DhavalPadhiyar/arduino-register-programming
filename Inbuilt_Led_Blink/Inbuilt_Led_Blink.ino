14a02755380e8f7788e1116705932f1fe8743424
Dhaval Padhiyar <dhavalpadhiyar18112004@gmail.com>
2025-12-18 18:27:46 +0530

    Update Inbuilt_Led_Blink.ino

--- Inbuilt_Led_Blink/Inbuilt_Led_Blink.ino
+++ Inbuilt_Led_Blink/Inbuilt_Led_Blink.ino
@@ -1,6 +1,6 @@
 void setup()
 { // inbuilt led of controller is connected to the PB5 pin/ digital pin 13 of controller
-  DDRB|=(1<<DDB5);// setting PB% as an output port
+  DDRB|=(1<<DDB5);// setting PB5 as an output port
 }
 
 void loop()
@@ -11,3 +11,4 @@ void loop()
   delay(1000);// 1s wait
 
 }
+
